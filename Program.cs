a63cf6c3bf9a8049832d78348e1f89f61c55373e
himanshu01031997 <hs010397@gmail.com>
2022-10-13 22:43:36 +0530

    added contact

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace AddressBook
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("welcome to address book");
+            Contact contact = new Contact()
+            {
+                FirstName = "Himanshu",
+                LastName="Singh",
+                Address="bsp",
+                City="bilaspur",
+                State="chhatisgarh",
+                Zip=45666,
+                Phoneno="989898999",
+                Email="hsjsak@"
+            };
+            Console.WriteLine("firstname:"+ contact.FirstName+"\n"+"Lastname:"+contact.LastName+"\n"+"Address:"+contact.Address+"\n"+"city:"+contact.City+"\n"+"State:"+contact.State+"\n"+"Zip:"+contact.Zip+"\n"+"Phoneno:"+contact.Phoneno+"\n"+"Email:"+contact.Email);
+
         }
     }
 }
