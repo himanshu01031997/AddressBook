50816dc9191d4b32bba6a47f9d70aa78700a77d5
himanshu01031997 <hs010397@gmail.com>
2022-10-14 12:25:37 +0530

    add contact

--- Contact.cs
+++ Contact.cs
@@ -8,18 +8,24 @@ namespace AddressBook
 {
     public class Contact
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
-        public string Address { get; set; }
-        public string City { get; set; }
-
-        public string State { get; set; }
-
-        public int Zip { get; set; }
-
-        public string Phoneno { get; set; }
-        public string Email { get; set; }
-
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public int Zip;
+        public string Phoneno;
+        public string Email;
+        public Contact(string firstName, string lastName, string address, string city, string state, int zip, string phoneno, string email)
+        {
+           this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+            this.Phoneno = phoneno;
+            this.Email = email;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,20 +4,17 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("welcome to address book");
-            Contact contact = new Contact()
-            {
-                FirstName = "Himanshu",
-                LastName="Singh",
-                Address="bsp",
-                City="bilaspur",
-                State="chhatisgarh",
-                Zip=45666,
-                Phoneno="989898999",
-                Email="hsjsak@"
-            };
-            Console.WriteLine("firstname:"+ contact.FirstName+"\n"+"Lastname:"+contact.LastName+"\n"+"Address:"+contact.Address+"\n"+"city:"+contact.City+"\n"+"State:"+contact.State+"\n"+"Zip:"+contact.Zip+"\n"+"Phoneno:"+contact.Phoneno+"\n"+"Email:"+contact.Email);
-
+            Contact contact = new Contact("Himanshu","Singh","bsnl","bsp","cg",76778,"89887776","hs@gmail.com");
+            Console.WriteLine("FirstName: " +contact.FirstName);
+            Console.WriteLine("LastName: "+contact.LastName);
+            Console.WriteLine("Address: "+contact.Address);
+            Console.WriteLine("City: " + contact.City);
+            Console.WriteLine("State: "+contact.State);
+            Console.WriteLine("Zip: "+contact.Zip);
+            Console.WriteLine("Phoneno: "+contact.Phoneno);
+            Console.WriteLine("Email:"+contact.Email);
+        
+        
         }
     }
 }
